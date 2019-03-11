db778ad1d1b7f0ca2a8911ab29712b9d3b39aa07
Rittika104 <48061714+Rittika104@users.noreply.github.com>
2019-03-11 14:46:52 +0530

    Add files via upload

---
+++ rime10.py
@@ -0,0 +1,11 @@
+try:
+	a=int(input("enter number"))
+	if a<=3:
+		b=a/a-3
+		print(b)
+    #raise Name error
+ except Name error:
+    #raise Zero division error
+ except Zero division error:
+ print ("some error occured")
+ print(u have choosen a number greater than 3)      
