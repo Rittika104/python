bc44b93d65070569ef18142dc0a9c55e318096a6
Rittika104 <48061714+Rittika104@users.noreply.github.com>
2019-03-25 14:40:55 +0530

    Add files via upload

---
+++ rime17.py
@@ -0,0 +1,3 @@
+Hii('i am Rittika raj')
+	print('i am studying in CSE')
+	print('i live in bangalore')
---
+++ rime18.py
@@ -0,0 +1,7 @@
+num=float(input("please Enter the Number"))
+if num>0:
+	print("positive number")
+elif num==0:
+	print("zero")
+else:
+	print("negative number")	
